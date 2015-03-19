b81e097263499a3d65a4454bd2d2ff0fd000b633
Robert Beckebans <robert.beckebans@gmail.com>
2015-03-19 01:27:44 +0100

    Tweaked shadowmap bias to fight shadow acne

--- neo/renderer/tr_local.h
+++ neo/renderer/tr_local.h
@@ -1073,6 +1073,8 @@ extern idCVar r_shadowMapLodBias;
 extern idCVar r_shadowMapPolygonFactor;
 extern idCVar r_shadowMapPolygonOffset;
 extern idCVar r_shadowMapOccluderFacing;
+extern idCVar r_shadowMapRegularDepthBiasScale;
+extern idCVar r_shadowMapSunDepthBiasScale;
 // RB end
 
 /*
