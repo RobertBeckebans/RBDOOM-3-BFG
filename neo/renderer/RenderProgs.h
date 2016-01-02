b536481272a31e04e6ceaf9f87998d706553f112
Robert Beckebans <robert.beckebans@gmail.com>
2016-01-02 23:59:44 +0100

    Added bilateral blur to SSAO

--- neo/renderer/RenderProgs.h
+++ neo/renderer/RenderProgs.h
@@ -465,6 +465,11 @@ public:
 		BindShader_Builtin( BUILTIN_AMBIENT_OCCLUSION );
 	}
 	
+	void	BindShader_AmbientOcclusionBlur()
+	{
+		BindShader_Builtin( BUILTIN_AMBIENT_OCCLUSION_BLUR );
+	}
+	
 #if 0
 	void	BindShader_ZCullReconstruct()
 	{
@@ -588,6 +593,7 @@ protected:
 		BUILTIN_SMAA_NEIGHBORHOOD_BLENDING,
 		
 		BUILTIN_AMBIENT_OCCLUSION,
+		BUILTIN_AMBIENT_OCCLUSION_BLUR,
 		// RB end
 		BUILTIN_STEREO_DEGHOST,
 		BUILTIN_STEREO_WARP,
