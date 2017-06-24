5a3ab34d125a8ca0d018323df4806e2ed970b999
Martin-Kevin Neumann <maddin081591@gmail.com>
2017-06-24 16:11:34 +0200

    fixed compilation error with gcc version 7.1.1

--- neo/idlib/StrStatic.h
+++ neo/idlib/StrStatic.h
@@ -102,7 +102,7 @@ public:
 	{
 		buffer[ 0 ] = '\0';
 		SetStaticBuffer( buffer, _size_ );
-		idStr::operator=( b );
+		idStr::operator=( idStr( b ) );
 	}
 	
 	ID_INLINE	explicit idStrStatic( const char c ) :
@@ -110,7 +110,7 @@ public:
 	{
 		buffer[ 0 ] = '\0';
 		SetStaticBuffer( buffer, _size_ );
-		idStr::operator=( c );
+		idStr::operator=( idStr( c ) );
 	}
 	
 	ID_INLINE	explicit idStrStatic( const int i ) :
@@ -118,7 +118,7 @@ public:
 	{
 		buffer[ 0 ] = '\0';
 		SetStaticBuffer( buffer, _size_ );
-		idStr::operator=( i );
+		idStr::operator=( idStr( i ) );
 	}
 	
 	ID_INLINE	explicit idStrStatic( const unsigned u ) :
@@ -126,7 +126,7 @@ public:
 	{
 		buffer[ 0 ] = '\0';
 		SetStaticBuffer( buffer, _size_ );
-		idStr::operator=( u );
+		idStr::operator=( idStr( u ) );
 	}
 	
 	ID_INLINE	explicit idStrStatic( const float f ) :
@@ -134,7 +134,7 @@ public:
 	{
 		buffer[ 0 ] = '\0';
 		SetStaticBuffer( buffer, _size_ );
-		idStr::operator=( f );
+		idStr::operator=( idStr( f ) );
 	}
 	
 private:
