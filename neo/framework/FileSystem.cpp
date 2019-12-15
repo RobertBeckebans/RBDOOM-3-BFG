26894ef98d4f6abd2413c5124c07e1d761a1e7da
MadDeCoDeR <gkwin8ac@hotmail.gr>
2019-12-15 17:56:32 +0200

    Keep one think at each request

--- neo/framework/FileSystem.cpp
+++ neo/framework/FileSystem.cpp
@@ -2884,11 +2884,10 @@ idFileSystemLocal::FindResourceFile
 */
 int idFileSystemLocal::FindResourceFile(const char* resourceFileName)
 {
-	const char* mapFileName = va("maps/%s", resourceFileName);
 	for (int i = 0; i < resourceFiles.Num(); i++)
 	{
 
-		if (idStr::Icmp(resourceFileName, resourceFiles[i]->GetFileName()) == 0 || idStr::Icmp(mapFileName, resourceFiles[i]->GetFileName()) == 0)
+		if (idStr::Icmp(resourceFileName, resourceFiles[i]->GetFileName()) == 0)
 		{
 			return i;
 		}
