bc1739e97563e052baa16c41ccff1025cb21a5a2
Robert Beckebans <robert.beckebans@gmail.com>
2025-05-27 20:14:42 +0200

    Updated clang-format-code.sh to use multithreading

--- neo/clang-format-code.sh
+++ neo/clang-format-code.sh
@@ -1,48 +1,41 @@
 #!/bin/sh
 
-# keep the following in sync with our supplied clang-format binaries!
-OUR_CLANGFMT_VERSION=11.0.0
+# Desired clang-format version
+OUR_CLANGFMT_VERSION=18.1.8
 
 print_usage () {
-	#echo "By default, this script only works on Linux i686 or amd64 (with our supplied clang-format binaries)"
-	#echo "You can use your own clang-format binary by setting the CLANGFMT_BIN environment variable before calling this script"
-	#echo "  e.g.: CLANGFMT_BIN=/usr/bin/clang-format $0"
-	echo "But please make sure it's version $OUR_CLANGFMT_VERSION because other versions may format differently!"
+    echo "Please ensure that clang-format version $OUR_CLANGFMT_VERSION is used, as other versions may produce different formatting!"
+    echo "Set the CLANGFMT_BIN environment variable to specify a custom path to clang-format."
+    echo "Example: CLANGFMT_BIN=/usr/bin/clang-format $0"
 }
 
-#if [ -z "$CLANGFMT_BIN" ]; then
-#
-#	if [ `uname -s` != "Linux" ]; then
-#		print_usage
-#		exit 1
-#	fi
+# Determine platform and set clang-format binary path
+case "$(uname -s)" in
+    Linux*)   CLANGFMT_DEFAULT="clang-format" ;;
+    MINGW*|MSYS*|CYGWIN*) CLANGFMT_DEFAULT="./clang-format.exe" ;;
+    *)        echo "ERROR: Unsupported platform: $(uname -s)"; print_usage; exit 1 ;;
+esac
 
-#	case "`uname -m`" in
-#		i?86 | x86 ) CLANGFMT_SUFFIX="x86" ;;
-#		amd64 | x86_64 ) CLANGFMT_SUFFIX="x86_64" ;;
-#		* ) print_usage ; exit 1  ;;
-#	esac
+# Use custom path or default path
+CLANGFMT_BIN=${CLANGFMT_BIN:-$CLANGFMT_DEFAULT}
 
-#	CLANGFMT_BIN="./clang-format.$CLANGFMT_SUFFIX"
-#fi
-
-CLANGFMT_BIN=clang-format
-
-#CLANGFMT_VERSION=$($CLANGFMT_BIN --version | grep -o -e "[[:digit:]\.]*")
-CLANGFMT_VERSION=$($CLANGFMT_BIN --version | egrep -o "([0-9]{1,}\.)+[0-9]{1,}")
-
-if [ "$CLANGFMT_VERSION" != "$OUR_CLANGFMT_VERSION" ]; then
-	echo "ERROR: $CLANGFMT_BIN has version $CLANGFMT_VERSION, but we want $OUR_CLANGFMT_VERSION"
-	echo "       (Unfortunately, different versions of clang-format produce slightly different formatting.)"
-	exit 1
+# Check if the binary exists and is executable
+if [ ! -x "$CLANGFMT_BIN" ]; then
+    echo "ERROR: $CLANGFMT_BIN not found or not executable"
+    print_usage
+    exit 1
 fi
 
-#clang-format -i renderer/RenderBackend.h
-#clang-format -i renderer/RenderBackend.cpp
+# Extract clang-format version
+CLANGFMT_VERSION=$($CLANGFMT_BIN --version | grep -o -E "([0-9]{1,}\.)+[0-9]{1,}")
 
-#clang-format -i aas/AASFile.h
-#clang-format -i aas/*
+if [ "$CLANGFMT_VERSION" != "$OUR_CLANGFMT_VERSION" ]; then
+    echo "ERROR: $CLANGFMT_BIN has version $CLANGFMT_VERSION, but version $OUR_CLANGFMT_VERSION is required"
+    echo "       (Different versions of clang-format may produce slightly different formatting.)"
+    exit 1
+fi
 
-find . -regex ".*\.\(cpp\|cc\|cxx\|h\|hpp\)" ! -path "./libs/*" ! -path "./d3xp/gamesys/SysCvar.cpp" ! -path "./d3xp/gamesys/Callbacks.cpp" ! -path "./sys/win32/win_cpu.cpp" ! -path "./sys/win32_win_main.cpp" -exec $CLANGFMT_BIN -i {} \;
+# Run clang-format on all relevant files
+find . -regex ".*\.\(cpp\|cc\|cxx\|h\|hpp\)" ! -path "./libs/*" ! -path "./extern/*" ! -path "./d3xp/gamesys/SysCvar.cpp" ! -path "./d3xp/gamesys/Callbacks.cpp" ! -path "./sys/win32/win_cpu.cpp" ! -path "./sys/win32/win_main.cpp" -print0 | xargs -0 -P 16 "$CLANGFMT_BIN" -i
 
-#find . -regex ".*\.\(c\|cpp\|cc\|cxx\|h\|hpp\)" ! -path "./libs/*" ! -exec $CLANGFMT_BIN -i {} \;
+echo "Formatting completed!"
